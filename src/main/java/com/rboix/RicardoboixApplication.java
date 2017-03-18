453c325cb971e8eece687ca314712b5febe8022e
Ana <hidalgo.ana@gmail.com>
2017-03-18 13:11:20 +0100

    Eliminar imports innecesarios

--- src/main/java/com/rboix/RicardoboixApplication.java
+++ src/main/java/com/rboix/RicardoboixApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication //configuracion
 
