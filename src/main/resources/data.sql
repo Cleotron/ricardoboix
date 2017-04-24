8992a44ad1dd3f2c918b18883c88d85765246c98
Ana <hidalgo.ana@gmail.com>
2017-04-24 20:18:01 +0200

    Añadir poema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1639,3 +1639,26 @@ Aquellas circunstancias cerraron definitivamente las puertas que tanto empeño t
 	
 	INSERT INTO comentarios(nombre, texto)
 	VALUES ('Alex', 'Otro comentario');
+	
+	INSERT INTO poemas(titulo, texto)
+	VALUES ('El poeta y el perro',
+		'Escribía un buen poeta 
+        Poemas con devoción
+        Pues en ellas él volcaba
+        Toda su imaginación
+
+        Fueron pasando los días
+        Y en el olvido quedaron                           
+        Muchas de sus poesías                    
+        A quien se las dedicaron                        
+
+        Pensando en lo acontecido              
+        Vio a un perrito abandonado        
+        Que también había perdido
+        El cariño de su amo
+
+        Había amor en su mirada       
+        El perro le fue más fiel 
+        Le dedicó sus caricias
+        Y se olvidó del papel'); 
+
